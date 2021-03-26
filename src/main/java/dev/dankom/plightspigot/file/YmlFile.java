8b8c586f28b09814daea5748205045e5a2594538
Danylo79 <dan.komisarenko@gmail.com>
2021-03-26 09:57:17 -0400

    Added yml file system

---
+++ src/main/java/dev/dankom/plightspigot/file/YmlFile.java
@@ -0,0 +1,142 @@
+package dev.dankom.plightspigot.file;
+
+import dev.dankom.plightspigot.PlightPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class YmlFile {
+    private File dataFolder;
+    private Logger logger;
+    private FileConfiguration dataConfig = null;
+    private File configFile = null;
+    private JavaPlugin plugin;
+    private String fileName;
+
+    public YmlFile(String fileName, PlightPlugin plugin) {
+        this.dataFolder = plugin.getDataFolder();
+        this.logger = plugin.getLogger();
+        this.fileName = fileName;
+        this.plugin = plugin;
+        logger.log(Level.INFO, "Yaml Config for " + fileName);
+        saveDefaultConfig();
+    }
+
+    public void reloadConfig() {
+        if (this.configFile == null) {
+            this.configFile = new File(dataFolder, fileName);
+        }
+
+        this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
+    }
+
+    public void set(String key, Object value) {
+        logger.log(Level.INFO, getConfig().toString());
+        this.dataConfig.set(key, value);
+        this.saveConfig();
+    }
+
+    public FileConfiguration getConfig() {
+        if (this.dataConfig == null)
+            reloadConfig();
+        return this.dataConfig;
+    }
+
+    public String getString(String path) {
+        return this.getConfig().getString(path);
+    }
+
+    public Integer getInt(String path) {
+        return getConfig().getInt(path);
+    }
+
+    public Boolean getBoolean(String path) {
+        return getConfig().getBoolean(path);
+    }
+
+    public List<String> getStringList(String path) {
+        return getConfig().getStringList(path);
+    }
+
+    public List<?> getList(String path) {
+        return getConfig().getList(path);
+    }
+
+    public Object get(String path) {
+        return getConfig().get(path);
+    }
+
+    public void saveConfig() {
+//        logger.log(Level.INFO, "Config Saving!");
+        if (this.dataConfig == null || this.configFile == null) {
+            return;
+        }
+        try {
+            this.getConfig().save(this.configFile);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "Could not save config to " + this.configFile, e);
+        }
+    }
+
+    private void saveDefaultConfig() {
+        if (this.configFile == null) {
+            this.configFile = new File(dataFolder, this.fileName);
+        }
+
+        if (!this.configFile.exists()) {
+            logger.log(Level.INFO, "Saved Default Config!");
+            saveResource(dataFolder, this.fileName, false);
+        }
+    }
+
+    public void saveResource(File dataFolder, String resourcePath, boolean replace) {
+        if (resourcePath == null || resourcePath.equals("")) {
+            throw new IllegalArgumentException("ResourcePath cannot be null or empty");
+        }
+
+        resourcePath = resourcePath.replace('\\', '/');
+        InputStream in = plugin.getResource(resourcePath);
+        if (in == null) {
+            throw new IllegalArgumentException("The embedded resource '" + resourcePath + "' cannot be found in ");
+        }
+
+        File outFile = new File(dataFolder, resourcePath);
+        int lastIndex = resourcePath.lastIndexOf('/');
+        File outDir = new File(dataFolder, resourcePath.substring(0, lastIndex >= 0 ? lastIndex : 0));
+
+        if (!outDir.exists()) {
+            outDir.mkdirs();
+        }
+
+        try {
+            if (!outFile.exists() || replace) {
+                OutputStream out = new FileOutputStream(outFile);
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                out.close();
+                in.close();
+            } else {
+                logger.log(Level.WARNING, "Could not save " + outFile.getName() + " to " + outFile + " because " + outFile.getName() + " already exists.");
+            }
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "Could not save " + outFile.getName() + " to " + outFile, ex);
+        }
+    }
+
+    public YmlSection createSection(String path) {
+        return new YmlSection(this, path);
+    }
+
+    public static YmlFile create(String name, PlightPlugin plugin) {
+        YmlFile file = new YmlFile(name, plugin);
+        return file;
+    }
+}
